647c1e740ff8eb441f2486c3528fd4fef5198ce3
Philipp Küng <git@philippkueng.ch>
2013-10-31 09:53:26 +0100

    changed DROPZONE_PATH; fixed resource_dict generator; removed the term_translation bit since not needed right now.

--- ckanext/stadtzhgeodropzone/harvesters/stadtzhgeodropzoneharvester.py
+++ ckanext/stadtzhgeodropzone/harvesters/stadtzhgeodropzoneharvester.py
@@ -36,96 +36,8 @@ class StadtzhgeodropzoneHarvester(HarvesterBase):
         'user': u'harvest'
     }
 
-    DROPZONE_PATH = u'/usr/lib/ckan/GEO'
+    DROPZONE_PATH = '/usr/lib/ckan/GEO'
 
-    # def _guess_format(self, file_name):
-    #     '''
-    #     Return the format for a given full filename
-    #     '''
-    #     _, file_extension = os.path.splitext(file_name.lower())
-    #     return file_extension[1:]
-
-    # def _generate_resources_dict_array(self, dataset_id):
-    #     '''
-
-    #     '''
-    #     try:
-    #         resources = []
-    #         prefix = self.DEPARTMENT_BASE + dataset_id + u'/'
-    #         bucket_list = self._get_s3_bucket().list(prefix=prefix)
-    #         for file in bucket_list:
-    #             if file.key != prefix:
-    #                 resources.append({
-    #                     'url': self.FILES_BASE_URL + '/' + file.key,
-    #                     'name': file.key.replace(prefix, u''),
-    #                     'format': self._guess_format(file.key)
-    #                     })
-    #         return resources
-    #     except Exception, e:
-    #         log.exception(e)
-    #         return []
-
-
-    # def _get_row_dict_array(self, lang_index):
-    #     '''
-    #     '''
-    #     try:
-    #         metadata_workbook = xlrd.open_workbook(self.METADATA_FILE_NAME)
-    #         worksheet = metadata_workbook.sheet_by_index(lang_index)
-
-    #         # Extract the row headers
-    #         header_row = worksheet.row_values(6)
-    #         rows = []
-    #         for row_num in range(worksheet.nrows):
-    #             # Data columns begin at row count 7 (8 in Excel)
-    #             if row_num >= 7:
-    #                 rows.append(dict(zip(header_row, worksheet.row_values(row_num))))
-    #         return rows
-
-    #     except Exception, e:
-    #         log.exception(e)
-    #         return []
-
-
-    # def _generate_term_translations(self, lang_index):
-    #     '''
-    #     '''
-    #     try:
-    #         translations = []
-
-    #         de_rows = self._get_row_dict_array(0)
-    #         other_rows = self._get_row_dict_array(lang_index)
-
-    #         log.debug(de_rows)
-    #         log.debug(other_rows)
-
-    #         keys = ['title', 'notes', 'author', 'maintainer', 'licence']
-
-    #         for row_idx in range(len(de_rows)):
-    #             for key in keys:
-    #                 translations.append({
-    #                     'lang_code': self.LANG_CODES[lang_index],
-    #                     'term': de_rows[row_idx][key],
-    #                     'term_translation': other_rows[row_idx][key]
-    #                     })
-
-    #             de_tags = de_rows[row_idx]['tags'].split(u', ')
-    #             other_tags = other_rows[row_idx]['tags'].split(u', ')
-
-    #             if len(de_tags) == len(other_tags):
-    #                 for tag_idx in range(len(de_tags)):
-    #                     translations.append({
-    #                         'lang_code': self.LANG_CODES[lang_index],
-    #                         'term': de_tags[tag_idx],
-    #                         'term_translation': other_tags[tag_idx]
-    #                         })
-
-    #         return translations
-
-
-    #     except Exception, e:
-    #         log.exception(e)
-    #         return []
 
     def _remove_hidden_files(self, file_list):
         '''
@@ -156,20 +68,20 @@ class StadtzhgeodropzoneHarvester(HarvesterBase):
         resource_files = self._remove_hidden_files(os.listdir(os.path.join(self.DROPZONE_PATH, dataset)))
         log.debug(resource_files)
 
-        for resource_file in resource_files:
-            if resource_file == u'meta.xml':
-                break
-            elif resource_file == u'link.xml':
+        # for resource_file in resource_files:
+        for resource_file in (x for x in resource_files if x != 'meta.xml'):
+            if resource_file == u'link.xml':
                 with open(os.path.join(self.DROPZONE_PATH, dataset, resource_file), 'r') as links_xml:
                     parser = etree.XMLParser(encoding='utf-8')
                     links = etree.fromstring(links_xml.read(), parser=parser).findall('link')
                     for link in links:
-                        resources.append({
-                            'url': link.find('url').text,
-                            'name': link.find('lable').text,
-                            'format': link.find('type').text,
-                            'resource_type': 'api'
-                        })
+                        if link.find('url').text != "":
+                            resources.append({
+                                'url': link.find('url').text,
+                                'name': link.find('lable').text,
+                                'format': link.find('type').text,
+                                'resource_type': 'api'
+                            })
             else:
                 resources.append({
                     'url': 'http://example.org/' + resource_file,
@@ -268,73 +180,6 @@ class StadtzhgeodropzoneHarvester(HarvesterBase):
                 ids.append(obj.id)
 
         return ids
-
-        # parser = etree.XMLParser(encoding='utf-8')
-        # for package in etree.fromstring(metadata_file.data, parser=parser):
-
-        #     # Get the german dataset if one is available, otherwise get the first one
-        #     base_datasets = package.xpath("dataset[@xml:lang='de']")
-        #     if len(base_datasets) != 0:
-        #         base_dataset = base_datasets[0]
-        #     else:
-        #         base_dataset = package.find('dataset')
-
-        #     metadata = self._generate_metadata(base_dataset, package)
-        #     if metadata:
-        #         obj = HarvestObject(
-        #             guid = base_dataset.get('datasetID'),
-        #             job = harvest_job,
-        #             content = json.dumps(metadata)
-        #         )
-        #         obj.save()
-        #         log.debug('adding ' + base_dataset.get('datasetID') + ' to the queue')
-        #         ids.append(obj.id)
-        #     else:
-        #         log.debug('Skipping ' + base_dataset.get('datasetID') + ' since no resources are available')
-
-
-
-        # self._fetch_metadata_file()
-        # ids = []
-
-        # de_rows = self._get_row_dict_array(0)
-        # for row in de_rows:
-        #     # Construct the metadata dict for the dataset on CKAN
-        #     metadata = {
-        #         'datasetID': row[u'id'],
-        #         'title': row[u'title'],
-        #         'notes': row[u'notes'],
-        #         'author': row[u'author'],
-        #         'maintainer': row[u'maintainer'],
-        #         'maintainer_email': row[u'maintainer_email'],
-        #         'license_id': row[u'licence'],
-        #         'translations': [],
-        #         'tags': row[u'tags'].split(u', '),
-        #         'groups': []
-        #     }
-
-        #     metadata['resources'] = self._generate_resources_dict_array(row[u'id'])
-        #     log.debug(metadata['resources'])
-
-        #     # Adding term translations
-        #     metadata['translations'].extend(self._generate_term_translations(1)) # fr
-        #     metadata['translations'].extend(self._generate_term_translations(2)) # it
-        #     metadata['translations'].extend(self._generate_term_translations(3)) # en
-
-        #     log.debug(metadata['translations'])
-
-        #     obj = HarvestObject(
-        #         guid = row[u'id'],
-        #         job = harvest_job,
-        #         content = json.dumps(metadata)
-        #     )
-        #     obj.save()
-        #     log.debug('adding ' + row[u'id'] + ' to the queue')
-        #     ids.append(obj.id)
-
-        #     log.debug(de_rows)
-
-        # return ids
 
 
     def fetch_stage(self, harvest_object):
