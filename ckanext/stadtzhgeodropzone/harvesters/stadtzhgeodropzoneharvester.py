57a1aac8359a8b98d8fd304dd7b6a475f33a4955
Rachel Knowler <rachel.knowler@liip.ch>
2014-10-06 15:01:09 +0200

    Set update interval and data type to blank value ('---') if not specified.

--- ckanext/stadtzhgeodropzone/harvesters/stadtzhgeodropzoneharvester.py
+++ ckanext/stadtzhgeodropzone/harvesters/stadtzhgeodropzoneharvester.py
@@ -222,7 +222,7 @@ class StadtzhgeodropzoneHarvester(HarvesterBase):
                             ('spatialRelationship', self._get(dataset_node, 'raeumliche_beziehung')),
                             ('dateFirstPublished', self._get(dataset_node, 'erstmalige_veroeffentlichung')),
                             ('dateLastUpdated', self._get(dataset_node, 'aktualisierungsdatum')),
-                            ('updateInterval', self._get(dataset_node, 'aktualisierungsintervall')),
+                            ('updateInterval', self._get(dataset_node, 'aktualisierungsintervall').replace(u'ä', u'ae').replace(u'ö', u'oe').replace(u'ü', u'ue')),
                             ('dataType', self._get(dataset_node, 'datentyp')),
                             ('legalInformation', self._get(dataset_node, 'rechtsgrundlage')),
                             ('version', self._get(dataset_node, 'aktuelle_version')),
@@ -259,6 +259,13 @@ class StadtzhgeodropzoneHarvester(HarvesterBase):
                         metadata['groups'] = groups
                 except AttributeError:
                     log.debug('No groups found for dataset %s.' % dataset)
+
+                for extra in list (metadata['extras']):
+                    if extra[0] == 'updateInterval' or extra[0] == 'dataType':
+                        if not extra[1]:
+                            metadata['extras'].append((extra[0], '---'))
+                            metadata['extras'].remove(extra)
+                            log.debug('No value in meta.xml for %s' % extra[0])
 
                 obj = HarvestObject(
                     guid = metadata['datasetID'],
